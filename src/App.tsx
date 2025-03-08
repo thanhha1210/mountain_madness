406a7d133fd8b177e25a5bbafbcd936c87286266
Nathan Dsouza <shawndsouza@d207-023-169-207.wireless.sfu.ca>
2025-03-08 15:11:49 -0800

    laptop

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import StartPage from './components/StartPage'
+import Test from './components/Test'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <StartPage />
+      <Test />
     </>
   )
 }
