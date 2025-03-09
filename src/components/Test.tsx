d616fa43c648c384fc068d25b6329d1820f4cadd
aidandevaal <aidan_de_vaal@sfu.ca>
2025-03-08 17:40:47 -0800

    added light button

--- src/components/Test.tsx
+++ src/components/Test.tsx
@@ -1,8 +1,34 @@
 import { Carousel } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Test.css';
-import laptopImg from '/Users/shawndsouza/SFU/Hackathon/Mountain Madness 2025/mountain_madness/src/assets/img/laptop.png';
+import laptopImg from '../assets/img/laptop.png';
 import StartPage from './StartPage';
+import lightButton from '../assets/img/lightButton.png';
+import lightButtonPushed from '../assets/img/lightButtonPress.png';
+
+export function resetLight() {
+  changeButton();
+  var time = new Date().getTime();
+  //do nothing
+  while(new Date().getTime() < time + 10000){
+      changeButton();
+  }
+}
+
+export function changeButton() {
+  if (idx === 0) {
+    idx = 1;
+  }
+  else {
+    idx = 0;
+  }
+}
+
+var idx = 0;
+const pictures = [
+  lightButton,
+  lightButtonPushed
+]
 
 function Test() {
   return (
@@ -27,7 +53,12 @@ function Test() {
       </Carousel.Item>
 
       <Carousel.Item>
-        <img src={laptopImg} alt="Third slide" />
+        <div>
+          <div className="lightButton">
+            <button className="light" onClick={() => resetLight()}></button>
+            <img src={pictures[idx]} alt="lightButton" />
+          </div>
+        </div>
       </Carousel.Item>
       
     </Carousel>
